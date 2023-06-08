070f68e20d67dd4922cbb9955c75ce296fea1934
Katya <katya.mikhaylova.98@list.ru>
2023-06-08 20:38:27 +0200

    fix: change img format

---
+++ src/Components/TypesBar/Icon.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { motion, MotionProps } from "framer-motion";
+
+function Icon() {
+  return (
+    <div></div>
+  )
+}
+
+export default Icon;
--- src/Components/TypesBar/index.tsx
+++ src/Components/TypesBar/index.tsx
@@ -6,31 +6,31 @@ function TypesBar() {
     <nav className={styles.typesbar}>
       <a>
         <img
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/FireIcon.svg/750px-FireIcon.svg.png"
+          src={process.env.PUBLIC_URL + "/Assets/types-icons/fire.png"}
           alt="fire"
         />
       </a>
       <a>
         <img
-          src="https://upload.wikimedia.org/wikipedia/commons/6/65/Icon-water-blue.svg"
+          src={process.env.PUBLIC_URL + "/Assets/types-icons/water.png"}
           alt="water"
         />
       </a>
       <a>
         <img
-          src="https://images.vexels.com/media/users/3/262042/isolated/preview/69326c8749e7a0bc882fbbe2a8e5fa50-leaf-botanical-icon.png"
+          src={process.env.PUBLIC_URL + "/Assets/types-icons/earth.png"}
           alt="earth"
         />
       </a>
       <a>
         <img
-          src="https://img.freepik.com/free-icon/wind_318-576755.jpg"
+          src={process.env.PUBLIC_URL + "/Assets/types-icons/air.png"}
           alt="air"
         />
       </a>
       <a>
         <img
-          src="https://cdn1.iconfinder.com/data/icons/black-and-white-magic/500/SingleOutlineBlackandWhiteMagicYulia_6-512.png"
+          src={process.env.PUBLIC_URL + "/Assets/types-icons/quintessence.png"}
           alt="quintessence"
         />
       </a>
