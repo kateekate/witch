da370bc3eee828fa1d8b4f8380073448a91ab8bf
Katya <katya.mikhaylova.98@list.ru>
2023-06-09 15:10:15 +0200

    feat: add animation to icons

--- src/Components/TypesBar/Icon.tsx
+++ src/Components/TypesBar/Icon.tsx
@@ -1,10 +1,62 @@
 import React from "react";
-import { motion, MotionProps } from "framer-motion";
+import styles from "./typesbar.module.css";
+import { motion } from "framer-motion";
 
 function Icon() {
   return (
-    <div></div>
-  )
+    <>
+      <motion.div
+        whileHover={{ scale: 1.5 }}
+        transition={{ type: "spring", stiffness: 900, damping: 10 }}
+        className={styles.icon}
+      >
+        <img
+          src={process.env.PUBLIC_URL + "/Assets/types-icons/fire.png"}
+          alt="fire"
+        />
+      </motion.div>
+      <motion.div
+        whileHover={{ scale: 1.6 }}
+        transition={{ type: "spring", stiffness: 900, damping: 10 }}
+        className={styles.icon}
+      >
+        <img
+          src={process.env.PUBLIC_URL + "/Assets/types-icons/water.png"}
+          alt="water"
+        />
+      </motion.div>
+      <motion.div
+        whileHover={{ scale: 1.6 }}
+        transition={{ type: "spring", stiffness: 900, damping: 10 }}
+        className={styles.icon}
+      >
+        <img
+          src={process.env.PUBLIC_URL + "/Assets/types-icons/earth.png"}
+          alt="earth"
+        />
+      </motion.div>
+      <motion.div
+        whileHover={{ scale: 1.6 }}
+        transition={{ type: "spring", stiffness: 900, damping: 10 }}
+        className={styles.icon}
+      >
+        <img
+          src={process.env.PUBLIC_URL + "/Assets/types-icons/air.png"}
+          alt="air"
+        />
+      </motion.div>
+      <motion.div
+        whileHover={{ scale: 1.6 }}
+        transition={{ type: "spring", stiffness: 900, damping: 10 }}
+        className={styles.icon}
+      >
+        <img
+          src={process.env.PUBLIC_URL + "/Assets/types-icons/quintessence.png"}
+          alt="quintessence"
+        />
+      </motion.div>
+    </>
+  );
 }
 
 export default Icon;
--- src/Components/TypesBar/index.tsx
+++ src/Components/TypesBar/index.tsx
@@ -1,39 +1,11 @@
 import React from "react";
 import styles from "./typesbar.module.css";
+import Icon from "./Icon";
 
 function TypesBar() {
   return (
     <nav className={styles.typesbar}>
-      <a>
-        <img
-          src={process.env.PUBLIC_URL + "/Assets/types-icons/fire.png"}
-          alt="fire"
-        />
-      </a>
-      <a>
-        <img
-          src={process.env.PUBLIC_URL + "/Assets/types-icons/water.png"}
-          alt="water"
-        />
-      </a>
-      <a>
-        <img
-          src={process.env.PUBLIC_URL + "/Assets/types-icons/earth.png"}
-          alt="earth"
-        />
-      </a>
-      <a>
-        <img
-          src={process.env.PUBLIC_URL + "/Assets/types-icons/air.png"}
-          alt="air"
-        />
-      </a>
-      <a>
-        <img
-          src={process.env.PUBLIC_URL + "/Assets/types-icons/quintessence.png"}
-          alt="quintessence"
-        />
-      </a>
+      <Icon />
     </nav>
   );
 }
